7464bad4e4bd6de50f5da8fc141dddb6372001dc
Andrew Duong <duongsters@gmail.com>
2019-09-25 16:50:36 -0700

    added if loop for errors during get receivement from the user

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 //import the keys.js file and stores it in 'keys' variable
 var keys = require("./keys.js");
 //access to my personal spotify api information
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 //access to my personal spotify api information
 var spotify = new Spotify(keys.spotify);
 //access to the filesystem
@@ -13,7 +13,7 @@ var fs = require("fs");
 //include the axios npm package stored within 'axios' variable
 var axios = require("axios");
 //include the moment npm package stored within 'moment' variable
-var moment = require("moment");
+var moment = require('moment');
 
 
 //grabs 'titleArtist' variable within the 3rd node of argument line
@@ -31,3 +31,19 @@ axios.get(queryUrl).then(
         console.log("Name of Artist: " + titleArtist  + "\nVenue Name: " + titleVenue + "\nVenue Location: "
          + locationVenue  + "Date Time: " + dateTimeVenue);
     })
+    .catch(function(error){
+        if (error.response) {
+            console.log(error.response.name);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        }
+        else if (error, request) {
+            //'error.request' is an object that comes back with details pertaining to the error that occured in receiving the request
+            console.log(error.request);
+        }
+        else {
+            //message the user that something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+            console.log(error.config);
+    });
