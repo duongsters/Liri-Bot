d4cf235022b15c1124372ce1b6c25e90c234f371
Andrew Duong <duongsters@gmail.com>
2019-09-25 16:34:08 -0700

    made changes within liri.js file

--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
+//read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-
+//import the keys.js file and stores it in 'keys' variable
 var keys = require("./keys.js");
+//access to my personal spotify api information
 var spotify = require("node-spotify-api");
-var spotify = new a
+//access to my personal spotify api information
+var spotify = new Spotify(keys.spotify);
