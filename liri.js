a863291d4b504355d245fc29c42d280a7107a069
Andrew Duong <duongsters@gmail.com>
2019-09-26 17:04:15 -0700

    removed my local variables within each function that were set to the third line of node and made it global variables...will start making switch cases on each function so it will render smoothly after I finish my spotify this portion right now

--- liri.js
+++ liri.js
@@ -2,10 +2,12 @@
 require("dotenv").config();
 //import the keys.js file and stores it in 'keys' variable
 var keys = require("./keys.js");
+
+
 //access to my personal spotify api information
 var spotify = new Spotify(keys.spotify);
 
-//-----------------------NPM Access-----------------------------------------------------------------------------------------------------------
+//-----------------------NPM Access--------------------------------------------------------------------------------------------------------------------------
 //access to the core node packafe to filesystem...for readi/writing files (used here for mainly the Do-What-It-Says portion)
 var fs = require("fs");
 //include the axios npm package stored within 'axios' variable
@@ -15,7 +17,16 @@ var moment = require('moment');
 //access to my personal spotify api information
 var Spotify = require("node-spotify-api");
 
-// ---------------------Bands In Town portion--------------------------------------------------------------------------------------------------
+//--------------------Global Variables----------------------------------------------------------------------------------------------------------------------
+var render = process.argv[2];
+var search = process.argv[3];
+
+
+//------------------------main renderments of liri.js--------------------------------------------------------------------------------------------------------
+
+
+// ---------------------Concert_This: Bands In Town portion--------------------------------------------------------------------------------------------------
+function renderBandsInTown (){
 //grabs 'titleArtist' variable within the 3rd node of argument line
 var titleArtist = process.argv[2];
 
@@ -28,9 +39,14 @@ axios.get(bandsUrl).then(
         var titleVenue = response.data[0].venue.name;
         var locationVenue = response.data[0].venue.city;
         var dateTimeVenue = response.data[0].datetime;
-        console.log("Name of Artist: " + titleArtist  + "\nVenue Name: " + titleVenue + "\nVenue Location: "
-         + locationVenue  + "\nDate Time: " + moment(dateTimeVenue).format("MM/DD/YYYY"));
-    })
+        console.log("\n----------------------------------------------------------------------------------------\n");
+        console.log("Name of Artist: " + titleArtist  + 
+                    "\nVenue Name: " + titleVenue + 
+                    "\nVenue Location: " + locationVenue + 
+                    // used moment within this line below to format the time to 'mm/dd/yyyy'
+                    "\nDate Time: " + moment(dateTimeVenue).format("MM/DD/YYYY"));
+         console.log("\n----------------------------------------------------------------------------------------\n");
+        })
     .catch(function(error){
         if (error.response) {
             console.log(error.response.name);
@@ -47,9 +63,12 @@ axios.get(bandsUrl).then(
         }
             console.log(error.config);
     });
+};
 
 
-// ----------------OMDb portion-----------------------------------------------------------------------------------------------------------------
+// ----------------Movie_This: OMDb portion-----------------------------------------------------------------------------------------------------------------
+function renderMovieThis (titleMovie) {
+// everything I used and implemented for the OMDb portion (line55-89) is similar coding to BandsInTown portion (lines20-49)
 //grabs 'titleMovie' variable within the 3rd node of argument line
 var titleMovie = process.argv[2];
 //'omdbUrl' variable to hold the url link to my OMDb API key & logging the responses to the console
@@ -65,12 +84,14 @@ axios.get(omdbUrl).then (
         var movieLanguage = response.data.Language;
         var moviePlot = response.data.Plot;
         var movieActors = response.data.Actors;
-
+        console.log("\n----------------------------------------------------------------------------------------\n");
         console.log("Title of the movie: " + titleMovie + "\nYear Movie Released: "
         + movieReleaseDate + "\nIMDB Rating: " + movieImdbRating + 
         "\nRotten Tomatoes Rating: " + movieTomatoesRating + "\nLocation of Movie Production: "
         + movieCountryProduction + "\nLanguage of Movie: " + movieLanguage
         + "\nMovie Plot: " + moviePlot + "\nActors in Movie: " + movieActors);
+        console.log("\n----------------------------------------------------------------------------------------\n");
+
     })
         .catch(function(error){
             if(error.response){
@@ -86,12 +107,15 @@ axios.get(omdbUrl).then (
             }
             console.log(error.config);
     });
+};
 
 
 // ------------------Do What It Says portion-------------------------------------------------------------------------------------------------
+
+function renderDoWhatItSays() {
 // The code will store the contents of the reading inside the variable "data" callback function
 // line 94 will begin starting to read from the file "random.txt"...'utf8' will construct the text it writes in random.txt as actually letters that we can read/understand
-fs.readFile("random.txt", "utf8", function(error, data) {
+fs.readFile("random.txt", "utf8", function (error, data) {
     // If the code experiences any errors it will log the error to the console.
     if (error) {
         return console.log(error);
@@ -100,11 +124,17 @@ fs.readFile("random.txt", "utf8", function(error, data) {
     console.log(data);
     // Then split it by commas (to make it more readable)
     var dataArr = data.split(",");
+    var spotifyLine = data.
     // We will then re-display the content as an array for later use.
     console.log(dataArr);
+
 });
+};
 
 
 
 
 //------------------Spotify This Song portion------------------------------------------------------------------------------------------------
+function renderSpotifyThisSong () {
+
+}
